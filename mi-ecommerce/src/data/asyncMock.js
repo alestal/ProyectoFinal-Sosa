26c58d7dc353add47e58ef972a7db7aa46cbe8ed
Alejandra Sosa Talento <irenealestal@gmail.com>
2024-12-21 22:26:11 -0300

    se modificaron archivos

--- mi-ecommerce/src/data/asyncMock.js
+++ mi-ecommerce/src/data/asyncMock.js
@@ -1,27 +1,33 @@
 const base_de_datos =[
     {   
         id:1,
-        name:"Anais Anais EDT 100ml",
-        price:100,
+        name:"ANAIS ANAIS EDT 100ml",
+        price: 100,
         category:"Perfumes",
         img: '/perfumes/anais-anais-edt-100ml.png',
         description:'Anaïs Anaïs es una composición en la que maravilla, frescura y ternura se armonizan juntas. Es una escritura olfativa revolucionaria convertida en un gran clásico de la perfumería con notas verdes y potentes que se basan en el jacinto, una celebración que hace gala al romanticismo y al primer amor, el sentimiento más bello y puro que existe.',
     },
+
      {   
         id:2,
         name:"ESPEJO DOBLE FACETA",
-        price:50,
+        price: 50,
         category:"Accesorios",
-        img: '/accesorios/espejo.jpg',
-        description:'Espejp doble faceta ideal para la cartera, retocar tu maquillaje estes donde estes',
-    }, {   
+        img: '/maquillaje/cuarteto-de-sombras.jpg',
+        description:'Espejo doble faceta ideal para la cartera, retocar tu maquillaje estes donde estes',
+    },
+        
+        {
         id:3,
         name:"CUARTETO DE SOMBRAS",
-        price:65,
-        category:"Maquillaje",
-        img: '/perfumes/anais-anais-edt-100ml.png',
-        description:'Paletas de sombras de ojos con el relieve del tejido, introducidas en una bolsita de tweed a juego con cada una de las armonías de color.',
-    }, 
+        price: 68,
+        category:"MAQUILLAJE",
+        img: '/maquillaje/cuarteto-de-sombras.jpg',
+        description:'Una gama de cuatro sombras de ojos de textura "polvo cocido". Un juego de colores a medida.Una armonía de tonos atemporales de acabado mate, satinado o metalizado que ofrece múltiples variaciones y contrastes. Un estuche lacado negro, práctico y nómada, con un amplio espejo integrado.Resultado: un maquillaje luminoso y de larga duración, monocromático o degradado, del más ligero al más intenso.',
+    },
+    
+
+   
   
     {   
         id:4,
@@ -34,8 +40,8 @@ const base_de_datos =[
     
     {   
         id:5,
-        name:"Necessaire",
-        price:45,
+        name:"NECESSAIRE",
+        price:89,
         category:"Accesorios",
         img: '/accesorios/necessaire.jpg',
         description:'Necessaire de viaje, bolsa de cosméticos de gran capacidad para mujeres, bolsa portátil impermeable, bolsa de aseo plana abierta, organizador de maquillaje con divisor y asa',
@@ -89,7 +95,7 @@ const base_de_datos =[
     {   
         id:11,
         name:"GOOD GIRL EDP 80 ML",
-        price:2,
+        price: 190,
         category:"Perfumes",
         img: '/perfumes/good-girl-edp-80ml.jpg',
         description:'Good Girl es una fragancia evocativa y sensual que nace de la hermosa contradicción y dualidad eterna de la mujer moderna. Revela tu lado bueno con el resplandor floral del jazmín, desafía tu lado malo con la sensualidad del haba tonka y cacao. Nunca ha sido tan bueno ser mala. Good Girl, una fragancia tan intensa como sensual para la mujer que celebra su lado bueno y libera su lado malo. El frasco está excepcionalmente único y moderno: un stiletto de color azul media noche con tacón dorado, expresa la naturaleza sensual, segura, femenina y poderosa de la mujer que lleva Good Girl.',
@@ -98,43 +104,86 @@ const base_de_datos =[
     {   
         id:12,
         name:"BROCHA KABUKI PETIT",
-        price:2,
-        category:"lAccesorios",
-        img: '/maquillaje/brocha-kabuki.webp',
+        price:57,
+        category:"Accesorios",
+        img: '/accesorios/brocha-kabuki.webp',
         description:'Diseñado para aplicar todo tipo de polvos. Su forma generosa permite modular la cobertura como se desee. Para un resultado de maquillaje a medida, totalmente natural. Están compuestos por una selección de pelo sintético, tan suave como resistente, y protegidos por una funda negra, para resistir al paso del tiempo con elegancia.',
     }, 
     
     {   
         id:13,
-        name:"l",
-        price:2,
-        category:"l",
-        img: '/perfumes/anais-anais-edt-100ml.png',
-        description:'k',
-    }, {   
-        id:1,
-        name:"l",
-        price:2,
-        category:"l",
-        img: '/perfumes/anais-anais-edt-100ml.png',
-        description:'k',
+        name:"MISS DIOR EDP 80 ML",
+        price:179,
+        category:"Perfumes",
+        img: '/perfumes/miss-dior-edp-80ml.jpg',
+        description:'Miss DIOR nació de esas veladas en Provenza, iluminadas por luciérnagas, en que el jazmín verde armoniza con la melodía de la noche y la tierra"". Así es como Christian DIOR explicaba la inspiración tras la creación de su primer perfume, Miss DIOR, en 1947. Francis Kurkdjian ha reinterpretado la fragancia Miss DIOR original con Miss DIOR Parfum. Un perfume femenino intenso, en el que predomina un jazmín con facetas afrutadas extraordinarias, envuelto con acordes amaderados ambarinos y salpicado de notas de mandarina. La traducción olfativa de un paseo nocturno por el corazón de un bosque tapizado de musgo, frutas jugosas y flores silvestres. Un motivo pata de gallo, emblemático de Miss DIOR, envuelve el frasco. Un lazo ""poignard"" plateado anudado en torno al cuello añade el toque final al espíritu couture de Miss DIOR Parfum.',
+    }, 
+    
+    {   
+        id:14,
+        name:"EYESHADOW SHADER BROCHA 21",
+        price:42,
+        category:"Accesorios",
+        img: '/accesorios/eyeshadow-brocha-21',
+        description:'Ofrece un resultado de maquillaje sublimado de forma sencilla. Permite un perfecto difuminado de las sombras de ojos.',
     },
 
     {   
-        id:1,
-        name:"l",
-        price:2,
-        category:"l",
-        img: '/perfumes/anais-anais-edt-100ml.png',
-        description:'k',
-    }, {   
-        id:1,
-        name:"l",
-        price:2,
-        category:"l",
-        img: '/perfumes/anais-anais-edt-100ml.png',
-        description:'k',
+        id:15,
+        name:"MASCARA INIMITABLE",
+        price:47,
+        category:"Maquillaje",
+        img: '/maquillaje/mascara-inimitable',
+        description:'Una máscara que ofrece un resultado de maquillaje 3D intenso. Un gesto que garantiza volumen, longitud, curvatura extrema y separación de las pestañas.La sinergia cepillo-fórmula potencia el efecto multidimensional:• Su cepillo de elastómero suave y flexible posee puntas ultra finas perfectamente ajustadas para una adherencia y una aplicación óptimas.• La fórmula recubre las pestañas para un resultado de maquillaje 3D intenso sin apelmazamiento.',
+    }, 
+    
+    {   
+        id:16,
+        name:"DIOR J'ADORE L'OR EDP 80 ML",
+        price:203,
+        category:"Perfumes",
+        img: '/perfumes/perfume-dior-jadore-lor-80ml.jpg',
+        description:'LOr de Jadore es el perfume para mujer con el que Francis Kurkdjian, Director Creativo de Perfumes DIOR, sublima la belleza de las flores del bouquet de Jadore al jugar con sus matices y colores. Las notas de los absolutos de flor de naranjo, jazmín grandiflorum y rosa centifolia se despliegan con plenitud y voluptuosidad: son el verdadero oro de Jadore. El frasco de LOr de Jadore se viste con el emblemático collar de J adore, elaborado en un metal precioso que abraza el cristal.',
     },
+
+    {   
+        id:17,
+        name:"SOMBAS DE OJOS Y RUBOR",
+        price:163,
+        category:"Maquillaje",
+        img: '/maquillaje/sombra-de-ojos-y-rubor.jpg',
+        description:'Inspirado en la luz incandescente de las auroras boreales y en los reflejos plateados de los copos de nieve escarchados para crear ENCHANTED NIGHT. Una paleta de edición limitada con cuatro nuevos tonos de sombra de ojos y rubor con acabados satinados nacarados. Un rosa fucsia, un top coat plateado, un coral cobrizo y un marrón púrpura brillante con reflejos azulados.',
+    },
+
+    {   
+        id:18,
+        name:"TOMMY GIRL EDT 100 ML",
+        price:57,
+        category:"Pefumes",
+        img: '/pefumes/tommy.jpg',
+        description:'Tommy Girl eau de toilette es una fragancia efervescente y floral que captura la libertad y el optimismo de un viaje por carretera en Estados Unidos. Los tesoros estadounidenses se abren con una explosión vigorizante de menta de origen natural y bergamota. En el corazón, un ramo de jazmín y rosa crea la sensación de pura felicidad. Finalmente, la fragancia revela un acorde sensual de sándalo cremoso, ámbar cálido y almizcle. Tommy Girl te abraza con un amor sin filtros y empoderamiento.',
+    },
+   
+    {   
+        id:20,
+        name:"MASCARA LE VOLUME INTENSE",
+        price:57,
+        category:"Maquillaje",
+        img: '/maquillaje/mascara-le-volume-intense.webp',
+        description:'Una máscara que ofrece un volumen inmediato desde la primera aplicación. Las pestañas se multiplican, cobran cuerpo y densidad. Revestidas de un color profundo, abren la mirada.La sinergia cepillo-fórmula permite un efecto de volumen intenso• El cepillo, que se ha desarrollado exclusivamente para CHANEL, esculpe el volumen gracias a sus discos superpuestos y orientados para un acabado óptimo.• La fórmula fluida de secado rápido permite crear un volumen a medida.',
+    },
+
+    {   
+        id:20,
+        name:"POISON EDT 100ML",
+        price:203,
+        category:"Perfumes",
+        img: '/perfumes/poison-edt-80ml',
+        description:'Algunos perfumes nacen siendo mitos. Provocador y misterioso desde su creación, Poison es el arma de seducción definitiva de DIOR. Su excepcional alquimia se orquesta alrededor de notas ricas, especiadas, florales y ambarinas, y de acordes sensuales que entremezclan la Miel y los Almizcles. Una fragancia oriental carismática e inolvidable. Los perfumes de la gama Poison rompen con todo y se atreven a provocar.Son perfumes creados para sorprender y para permitir a las mujeres asumir y reafirmar su feminidad.',
+    },
+
+
+
 ]
 
 export const getProducts = () => {
@@ -148,3 +197,8 @@ export const getProducts = () => {
 
     )
 }
+
+export const getProduct = (name) => {
+    return products.find((prod) => prod.id == id);
+  };
+  
